95024003546ce4e5bf3c65d195573a12d5a56a7c
Anders Perrone <perronea@ohsu.edu>
2020-12-08 16:47:44 -0800

    Clone v1.0.0 of SwipesForScience

---
+++ src/components/Terms.vue
@@ -0,0 +1,21 @@
+<template name="terms">
+  <div id="terms">
+    Input your terms and conditions (e.g. a consent form) here.
+  </div>
+</template>
+<style>
+</style>
+<script>
+/**
+ * **TODO**: This should be a researcher's consent form. It should come from a
+ * a URL to a markdown file described in the `config` from the parent component.
+ */
+  export default {
+    name: 'terms',
+    data() {
+      return {
+
+      };
+    },
+  };
+</script>
